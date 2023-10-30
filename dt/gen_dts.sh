9f5c90fe57931006243853345362d49cb72fdaea
Manfred SCHLAEGL <manfred.schlaegl@gmx.at>
2023-10-30 12:37:10 +0100

    dt: harmonized device-tree with hifive-unleashed from linux-6.6
    covers:
     * structure
     * compatible strings
     * removed clint node
     * settings of cores, plic
     * cpu-map
     * rv64 mmu: sv39 instead of sv48
    
    cache related settings are left for the future

--- dt/gen_dts.sh
+++ dt/gen_dts.sh
@@ -10,8 +10,9 @@ usage() {
 cd $(dirname $0)
 
 if [[ $TYPE == rv32 ]] ; then
+	RISCV_ISA_CPU0="rv32imac"
 	RISCV_ISA="rv32imafdc"
-	MMU_TYPE="riscv-sv32"
+	MMU_TYPE="riscv,sv32"
 	MEM_SIZE="0x40000000"	# 1 GiB
 
 	# on RV32 linux vmalloc size is very limited
@@ -20,8 +21,9 @@ if [[ $TYPE == rv32 ]] ; then
 	MRAM_SIZE="0x4000000"	# 64MiB
 
 elif [[ $TYPE == rv64 ]] ; then
+	RISCV_ISA_CPU0="rv64imac"
 	RISCV_ISA="rv64imafdc"
-	MMU_TYPE="riscv-sv48"
+	MMU_TYPE="riscv,sv39"
 	MEM_SIZE="0x80000000"	# 4GiB
 	ROOTFSTYPE="romfs"
 	MRAM_SIZE="0x20000000"	# 512MiB
@@ -39,23 +41,19 @@ fi
 
 CPUS_TEMP=$(mktemp)
 
-CLINT_INT_EXT=""
-PLIC_INT_EXT=""
-status="disabled"
-for i in $(seq 0 $NUM_CORES) ; do
+PLIC_INT_EXT="\n\t\t\t\t<\&cpu0_intc 0xffffffff>"
+CPU_MAP=""
+for cpu_nr in $(seq 1 $NUM_CORES) ; do
 	cat linux-vp_cpu.dts.in | sed			\
-		-e "s/@CPU_NR@/$i/g"			\
-		-e "s/@STATUS@/\"$status\"/g"		\
+		-e "s/@CPU_NR@/$cpu_nr/g"		\
 		-e "s/@RISCV_ISA@/\"$RISCV_ISA\"/g"	\
 		-e "s/@MMU_TYPE@/\"$MMU_TYPE\"/g"	\
 		>> $CPUS_TEMP
-	CLINT_INT_EXT+="\n\t\t\t\t\&CPU${i}_intc 3 \&CPU${i}_intc 7"
-	PLIC_INT_EXT+="\n\t\t\t\t\&CPU${i}_intc 11"
-	if [[ $i != 0 ]] ; then
-		PLIC_INT_EXT+=" \&CPU${i}_intc 9"
-	fi
-	# first cpu is always disabled
-	status="okay"
+	CPU_MAP+="\n"
+	CPU_MAP+="\t\t\t\tcore${cpu_nr} {\n"
+	CPU_MAP+="\t\t\t\t\tcpu = <\&cpu${cpu_nr}>;\n"
+	CPU_MAP+="\t\t\t\t};\n"
+	PLIC_INT_EXT+=",\n\t\t\t\t<\&cpu${cpu_nr}_intc 0xffffffff>, <\&cpu${cpu_nr}_intc 9>"
 done
 
 cat linux-vp_base.dts.in | sed \
@@ -63,6 +61,9 @@ cat linux-vp_base.dts.in | sed \
 		s/\@CPUS\@//g
 		r $CPUS_TEMP
 	}" \
+	-e "s/@RISCV_ISA_CPU0@/\"$RISCV_ISA_CPU0\"/g"	\
+	-e "s/@RISCV_ISA@/\"$RISCV_ISA\"/g"		\
+	-e "s/@CPU_MAP@/$CPU_MAP/g"			\
 	-e "s/@ROOTFSTYPE@/$ROOTFSTYPE/g"		\
 	-e "s/@MEM_SIZE@/$MEM_SIZE/g"			\
 	-e "s/@MRAM_SIZE@/$MRAM_SIZE/g"			\
